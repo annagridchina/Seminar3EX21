7bafb65b094e7852fe4fbc477f40bbacfc471ee5
anna <annagridchina@mail.ru>
2022-12-24 19:51:33 +0700

    чушь

--- Program.cs
+++ Program.cs
@@ -7,4 +7,19 @@
 
 //A (2,4); B (0,7) -> ~3.6
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Start");
+int xa = 0;
+int ya = 0;
+int xb = 1;
+int yb = 1;
+Console.SetCursor.Position (xa, ya);
+
+Console.SetCursor.Position (xb, yb);
+Console.WriteLine("*");
+
+
+
+
+
+
+Console.WriteLine("END");
